cf26c5d62d506416999995f3a1b03c09a3b73c8b
marcelodacosta-br <82119927+marcelodacosta-br@users.noreply.github.com>
2021-06-17 11:43:54 -0300

    Meu primeiro commit c#

---
+++ OlaMundo/OlaMundo/OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo, meu primeiro código C# 2021");
+        }
+    }
+}
